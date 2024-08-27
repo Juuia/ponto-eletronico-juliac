845d4223c3874685351a4e62fe4dd323c6cce46c
Juuia <juliaacarvalhoo@sempreceub.com>
2024-08-27 09:52:59 -0300

    atualizado

--- js/index.js
+++ js/index.js
@@ -1,3 +1,21 @@
 const diaSemana = document.getElementById("dia-semana");
 const diaMesAno = document.getElementById("dia-mes-ano");
 const horaMinSeg = document.getElementById("hora-min=seg");
+
+diaSemana.textContent = "Domingoooooooooo"
+diaMesAno.textContent = getCurrentDate();
+
+function getCurrentHour() {
+    // getHours()
+    // getMinutes()
+    // getSeconds()
+    // separados por ":"
+    // todos com dois dígitos (se menor que 10, 0 a esquerda)
+    // retorna hora:minuto:segundo
+}
+
+function getCurrentDate() {
+    const date = new Date();
+    return date.getDate() + "/" + date.getMonth() + "/" + date.getFullYear();
+}
+
