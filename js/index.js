b5171fca8dd6afd9a1510b34ebcfe7efb5d6734d
Juuia <juliaacarvalhoo@sempreceub.com>
2024-08-27 10:33:09 -0300

    tem que arrumar

--- js/index.js
+++ js/index.js
@@ -1,21 +1,46 @@
 const diaSemana = document.getElementById("dia-semana");
 const diaMesAno = document.getElementById("dia-mes-ano");
-const horaMinSeg = document.getElementById("hora-min=seg");
+const horaMinSeg = document.getElementById("hora-min-seg");
 
-diaSemana.textContent = "Domingoooooooooo"
+diaSemana.textContent = getWeekDay();
 diaMesAno.textContent = getCurrentDate();
 
+
+function getWeekDay() {
+    const date = new Date();
+    let days = ["Domingo", "Segunda-feira", "Terça-feira", "Quarta-feira", "Quinta-feira", "Sexta-feira", "Sábado"];
+    return days[date.getDay()];
+}
+
 function getCurrentHour() {
+    const date = new Date();
+    return date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds();
+ 
+    }
     // getHours()
     // getMinutes()
     // getSeconds()
     // separados por ":"
     // todos com dois dígitos (se menor que 10, 0 a esquerda)
     // retorna hora:minuto:segundo
+
+function printCurrentHour() {
+    horaMinSeg.textContent = getCurrentHour();
+
 }
+
 
 function getCurrentDate() {
     const date = new Date();
     return date.getDate() + "/" + date.getMonth() + "/" + date.getFullYear();
 }
 
+setInterval
+
+//const date = new Date();
+//const options = {
+//    month: '2-digit',
+//    day: '2-digit',
+//    year: 'numeric'
+//};
+//    return date.toLocaleDateString();
